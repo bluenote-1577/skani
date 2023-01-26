1ae5a6f3cff41062404f80c3ccd8b77afe5d775e
jim <jimshawster@gmail.com>
2023-01-25 22:02:37 -0500

    fixed underestimating AF

--- src/chain.rs
+++ src/chain.rs
@@ -216,11 +216,8 @@ fn calculate_ani(
             continue;
         }
 
-        total_query_bases += total_range_query.1 - total_range_query.0;
-        total_ref_range += total_range_query.1 - total_range_query.0;
-
-        //        total_query_bases += total_bases_contained_query;
-                //total_ref_range += total_bases_contained_ref;
+        total_query_bases += total_range_query.1 - total_range_query.0 + 2 * c + map_params.k as GnPosition;
+        total_ref_range += total_range_query.1 - total_range_query.0 + 2 * c + map_params.k as GnPosition;
 
         let mut num_seeds_in_intervals = 0;
         let mut upper_lower_seeds = 0;
