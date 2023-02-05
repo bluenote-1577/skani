645a36002e08d03c63f98bf1614d488a25d4e19d
jim <jimshawster@gmail.com>
2023-02-05 13:20:19 -0500

    fixed model script

--- model_to_src.sh
+++ model_to_src.sh
@@ -1,4 +1,4 @@
-MODEL=./skani-mummer-train/src/models/0.18665557-5-115-0.12.model
+MODEL=skani-mummer-train/good_models/0.18665557-5-115-0.12.model
 MODEL_C200=skani-mummer-train/good_models/0.18635842-7-230-0.08.model
 SRC=src/model.rs
 
